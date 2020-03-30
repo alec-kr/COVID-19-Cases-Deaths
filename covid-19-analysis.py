6ae329df5e99289e668db45a162603f206493697
alec-kr <52685467+alec-kr@users.noreply.github.com>
2020-03-30 14:00:43 -0400

    Update covid-19-analysis.py

--- covid-19-analysis.py
+++ covid-19-analysis.py
@@ -2,27 +2,28 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+def display_graph():
+    plt.plot(days, cases, color = "blue")
+    plt.grid(color="gray")
+    plt.title("Line Graph Showing Cases/Deaths for "+u_input)
+    plt.plot(days, deaths, color = "red")
+    plt.legend(["Cases","Deaths"])
+    plt.xlabel("Days")
+    plt.ylabel("# of Cases/Deaths")
+    print("Plot Loaded")
+    plt.show()
+    print("Plot closed")
+
 # Import Data From csv
 df = pd.read_csv(r"https://covid.ourworldindata.org/data/ecdc/full_data.csv")
 
-country = input("Please Enter a Country: ").title()
+u_input = input("Please Enter a Country: ").title()
 
-# Gather the records related to the specific country
+# Gather the records related to the specific u_input
 print("Loading info...")
 
-location = df[df["location"] == country]
-days = location["date"]
-cases = location["total_cases"]
-deaths = location["total_deaths"]
+country = df[df["location"] == u_input]
+days, cases, deaths = country["date"], country["total_cases"], country["total_deaths"]
 
 # Plot the trends on a line graph
-plt.plot(days, cases, color = "blue")
-plt.grid(color="gray")
-plt.title("Line Graph Showing Cases/Deaths for "+country)
-plt.plot(days, deaths, color = "red")
-plt.legend(["Cases","Deaths"])
-plt.xlabel("Days")
-plt.ylabel("# of Cases/Deaths")
-plt.show()
-
-print("Plot closed")
+display_graph()
