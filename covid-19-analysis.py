04201b5b5dc2fdbfa0fb5e67896053607581c8d6
alec-kr <52685467+alec-kr@users.noreply.github.com>
2020-03-30 15:02:08 -0400

    Update covid-19-analysis.py

--- covid-19-analysis.py
+++ covid-19-analysis.py
@@ -2,10 +2,24 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# Import Data From csv
+try:
+    df = pd.read_csv(r"https://covid.ourworldindata.org/data/ecdc/full_data.csv")
+
+except:
+    print("ERROR: The requested CSV may have been moved or deleted. Please visit https://covid.ourworldindata.org to locate a new data source.")
+
+def get_data(u_input):
+    print("Loading info...")
+    country_data = df[df["location"] == u_input]
+    global days, cases, deaths
+    days, cases, deaths = country_data["date"], country_data["total_cases"], country_data["total_deaths"]
+
+# Plot the trends
 def display_graph():
     plt.plot(days, cases, color = "blue")
     plt.grid(color="gray")
-    plt.title("Line Graph Showing Cases/Deaths for "+u_input)
+    plt.title("Line Graph Showing Cases/Deaths for "+user_sel)
     plt.plot(days, deaths, color = "red")
     plt.legend(["Cases","Deaths"])
     plt.xlabel("Days")
@@ -14,16 +28,7 @@ def display_graph():
     plt.show()
     print("Plot closed")
 
-# Import Data From csv
-df = pd.read_csv(r"https://covid.ourworldindata.org/data/ecdc/full_data.csv")
+user_sel = input("Please Enter a Country: ").title()
 
-u_input = input("Please Enter a Country: ").title()
-
-# Gather the records related to the specific u_input
-print("Loading info...")
-
-country = df[df["location"] == u_input]
-days, cases, deaths = country["date"], country["total_cases"], country["total_deaths"]
-
-# Plot the trends on a line graph
+get_data(user_sel)
 display_graph()
