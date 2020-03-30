08d71869246c79084dc529c6e6c7386da5c39f48
alec-kr <52685467+alec-kr@users.noreply.github.com>
2020-03-30 15:14:50 -0400

    Update covid-19-analysis.py

--- covid-19-analysis.py
+++ covid-19-analysis.py
@@ -9,12 +9,12 @@ try:
 except:
     print("ERROR: The requested CSV may have been moved or deleted. Please visit https://covid.ourworldindata.org to locate a new data source.")
 
+# Gather the cases/deaths for the country
 def get_data(u_input):
     print("Loading info...")
     country_data = df[df["location"] == u_input]
     global days, cases, deaths
     days, cases, deaths = country_data["date"], country_data["total_cases"], country_data["total_deaths"]
-    print("Alec Ramdhan")
 
 # Plot the trends
 def display_graph():
