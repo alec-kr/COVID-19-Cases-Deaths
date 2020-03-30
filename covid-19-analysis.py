eb907a8bf16a572cba6b9fb23ac943b6038b2c95
alec-kr <52685467+alec-kr@users.noreply.github.com>
2020-03-30 15:13:38 -0400

    Update covid-19-analysis.py

--- covid-19-analysis.py
+++ covid-19-analysis.py
@@ -14,6 +14,7 @@ def get_data(u_input):
     country_data = df[df["location"] == u_input]
     global days, cases, deaths
     days, cases, deaths = country_data["date"], country_data["total_cases"], country_data["total_deaths"]
+    print("Alec Ramdhan")
 
 # Plot the trends
 def display_graph():
