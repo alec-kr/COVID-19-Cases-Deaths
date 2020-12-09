77cd0cff51100d6e9747502f18c17a968a0c30a2
alec-kr <52685467+alec-kr@users.noreply.github.com>
2020-12-08 23:08:19 -0400

    Made minor updates to covid-19-analysis.py

--- covid-19-analysis.py
+++ covid-19-analysis.py
@@ -1,13 +1,12 @@
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import mplcursors
-# import math
-
+ 
 # Import data from an online CSV file
 try:
     df = pd.read_csv(r"https://covid.ourworldindata.org/data/ecdc/full_data.csv")
 
+# If no connection can be established, run the visualization on an old data set, and notify the user.
 except:
     print("ERROR: The requested CSV may have been moved or deleted. Please visit https://covid.ourworldindata.org to locate a new data source.")
 
@@ -23,21 +22,23 @@ def display_graph():
     plt.figure(figsize=(15,8))
     plt.rcParams.update({'font.size': 9})
     plt.plot(days, cases, color = "blue")
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=90)
     plt.grid(color="gray")
     plt.title("Line Graph Showing Cases/Deaths for "+user_sel)
     plt.plot(days, deaths, color = "red")
     plt.legend(["Cases","Deaths"])
-    # yint = range(min(cases), math.ceil(max(cases))+1)
-    # plt.yticks(yint)
+
     plt.xlabel("Days")
     plt.ylabel("# of Cases/Deaths")
     print("Plot Loaded")
-    
+
     mplcursors.cursor(hover = True) # Add hovering cursor
     plt.show()
+
+    # Print a messgage after the plot window is closed
     print("Plot closed")
 
+# Ask for an input, and set the first letter to uppercase
 user_sel = input("Please Enter a Country: ").title()
 
 get_data(user_sel)
